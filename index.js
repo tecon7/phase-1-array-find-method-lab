1d52696c53f77161e7fe5056b263bea95adc5357
Teddy Economou <teddyecon@gmail.com>
2023-01-12 23:01:25 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+function superbowlWin(record){
+    for (let champs of record) {
+        if(champs.result === "W"){
+            return champs.year
+        }
+    }
+}
